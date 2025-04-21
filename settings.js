53b5b9089c0819cce58709d4597fe3887ee1ceb8
ritu667 <rick8910065@gmail.com>
2025-04-22 03:26:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "🦋⃟ᴠͥɪͣᴘͫ✮⃝R𝄟⃝I✮⃝C𝄟⃝K",
   botOwner: '🐰⃝»̶̶͓͓͓̽̽̽⑅⃝༊𝐂𝐑𝐔𝐒𝐇 𝗞𝗜𝗡𝗚 »̶̶͓͓͓̽̽̽⑅⃝🩵🪽', // Your name
-  ownerNumber: '918910065100', //Your number
+  ownerNumber: '919830858093', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
